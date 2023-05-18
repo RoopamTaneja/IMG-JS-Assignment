80af8e7cf0a800d663786add0c375f622f6789ab
RoopamTaneja <roopamtaneja999@gmail.com>
2023-05-18 13:30:46 +0530

    almost completed post request part

--- fancy table/index.js
+++ fancy table/index.js
@@ -6,6 +6,7 @@ p.then((value) => {
     .then((obj_data) => {
         let tableData = "";
         for (let value of obj_data.data) {
+            //of not in and use obj_data.data bcoz the objects are in data property of obj_data
             tableData += `<tr> <td>${value.id}</td> <td><img src = "${value.avatar}"></img></td> <td>${value.first_name}</td> <td>${value.last_name}</td>  <td>${value.email}</td> </tr>`;
             //remember backticks and no single quotes
             document.getElementById('table_body').innerHTML = tableData;
--- signin box/index.js
+++ signin box/index.js
@@ -1,7 +1,7 @@
-// Error handling : 
+// Validations : 
 
 function emailCheck(data) {
-    let check = new RegExp(/^\w+@[a-z]{2,}\.([a-z]{3}|[a-z]{2}\.[a-z]{2})$/);
+    let check = new RegExp(/^.+@[a-z]{2,}\.([a-z]{2,3}|[a-z]{2}\.[a-z]{2})$/);
     let errorMessage = document.querySelector('#email-error-message');
 
     if (check.test(data) || data == '') {
@@ -35,4 +35,47 @@ function passCheck() {
     }
 }
 
-// Fetch API
+// Fetch API:
+
+
+//testing : 
+// let p = fetch('https://reqres.in/api/login', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({ "email": "eve.holt@reqres.in", "password": "cityslicka" })
+// });
+// p.then((res) => {
+//     return res.json();
+// })
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+
+const myForm = document.getElementById('myForm');
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.querySelector('.email').value;
+    const password = document.querySelector('#entpass').value;
+    const data = { email, password };
+    let p = fetch('https://reqres.in/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+})
